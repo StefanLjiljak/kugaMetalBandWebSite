1e7546b7a2286df40117d47eeecd57e088ff2f7e
StefanLjiljak <stefanljiljak@gmail.com>
2020-04-23 19:55:12 +0200

    svakog dana u svakom pogledu sve vise napredujem ;)

---
+++ main.js
@@ -0,0 +1,17 @@
+// let albums = document.querySelectorAll('section#discography ul li img');
+
+// albums.forEach(album => {
+//     album.addEventListener('mouseover', (e) => {
+//         e.target.style.width = '6rem';
+//         e.target.style.height = '6rem';
+
+//         setTimeout(() => {
+//             e.target.style.width = '3rem';
+//             e.target.style.height = '3rem';
+//         }, 1500);
+//         clearTimeout();
+//     })
+// });
+
+
+
